9248a03d5df1065e6fb06691f06207c3179bc1a6
Salvo <cappuccinosalvatore@gmail.com>
2019-07-06 22:10:31 +0200

    - Added default page and style - Added incons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: 'add', redirectTo: '' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
