2e792aaef1eed720e9b3ec5b11d91190d749181d
Salvo <cappuccinosalvatore@gmail.com>
2019-07-17 12:51:15 +0200

    - Added filter component

--- src/app/products/products-filter/products-filter.component.ts
+++ src/app/products/products-filter/products-filter.component.ts
@@ -1,15 +1,126 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Category } from '../category.model';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-products-filter',
   templateUrl: './products-filter.component.html',
-  styleUrls: ['./products-filter.component.scss']
+  styleUrls: [
+    '../../../assets/scss/form/form.scss',
+    './products-filter.component.scss',
+    '../../../assets/scss/mediaquery.scss'
+  ]
 })
 export class ProductsFilterComponent implements OnInit {
+  showFilters = false;
 
-  constructor() { }
+  categories: Category[];
+
+  filtersForm: FormGroup;
+  genders = ['None', 'Man', 'Woman', 'Unisex'];
+  // all the params map the query string for the api
+  initialFiltersFormValue = {
+    availability: false,
+    gender: '',
+    categoryId: '',
+    cost_gte: '',
+    cost_lte: '',
+    rating: '',
+    // q: api param for full text search
+    q: null
+  };
+
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.productsService.fetchCategories().subscribe((categoriesData) => {
+      this.categories = categoriesData;
+    });
+
+    this.initForm();
   }
 
+  initForm() {
+    this.filtersForm = new FormGroup(
+      {
+        availability: new FormControl(
+          this.route.snapshot.queryParamMap.get('availability') ||
+            this.initialFiltersFormValue.availability
+        ),
+        gender: new FormControl(
+          this.route.snapshot.queryParamMap.get('gender') ||
+            this.initialFiltersFormValue.gender
+        ),
+        categoryId: new FormControl(
+          this.route.snapshot.queryParamMap.get('categoryId') ||
+            this.initialFiltersFormValue.categoryId
+        ),
+        q: new FormControl(
+          this.route.snapshot.queryParamMap.get('q') ||
+            this.initialFiltersFormValue.q
+        ),
+        rating: new FormControl(
+          this.route.snapshot.queryParamMap.get('rating') ||
+            this.initialFiltersFormValue.rating
+        ),
+        cost_gte: new FormControl(
+          this.route.snapshot.queryParamMap.get('cost_gte') ||
+            this.initialFiltersFormValue.cost_gte
+        ),
+        cost_lte: new FormControl(
+          this.route.snapshot.queryParamMap.get('cost_lte') ||
+            this.initialFiltersFormValue.cost_lte
+        )
+      },
+      {
+        validators: [this.costValidator]
+      }
+    );
+  }
+
+  onApplyFilters() {
+    this.cleanEmptyValues(this.filtersForm.value);
+
+    this.router.navigate(this.route.snapshot.url, {
+      queryParams: this.filtersForm.value
+    });
+
+    this.showFilters = false;
+  }
+
+  onResetFilters() {
+    this.filtersForm.reset(this.initialFiltersFormValue);
+
+    this.onApplyFilters();
+  }
+
+  cleanEmptyValues(filtersFormValues) {
+    for (const key in filtersFormValues) {
+      if (
+        filtersFormValues.hasOwnProperty(key) &&
+        (filtersFormValues[key] === false ||
+          filtersFormValues[key] === '' ||
+          filtersFormValues[key] === null)
+      ) {
+        delete filtersFormValues[key];
+      }
+    }
+  }
+
+  costValidator(group: FormGroup): { [s: string]: boolean } | null {
+    if (+group.value.cost_gte > +group.value.cost_lte) {
+      group.controls.cost_lte.setErrors({ lesser: 'lesser then "Price from"' });
+      group.controls.cost_gte.setErrors({ greater: 'greater then "Price to"' });
+      return { notvalid: true };
+    }
+    group.controls.cost_lte.setErrors(null);
+    group.controls.cost_gte.setErrors(null);
+    return null;
+  }
 }
--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
-import { Category } from '../category.model';
 import { Product } from '../product.model';
 import { ProductsService } from '../products.service';
 
@@ -10,41 +8,20 @@ import { ProductsService } from '../products.service';
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: [
-    '../../../assets/scss/form/form.scss',
     './products-list.component.scss',
     '../../../assets/scss/mediaquery.scss'
   ]
 })
 export class ProductsListComponent implements OnInit, OnDestroy {
   products: Product[];
-  categories: Category[];
-
-  showFilters = false;
-  filtersForm: FormGroup;
-
-  // all the params map the query string for the api
-  initialFiltersFormValue = {
-    availability: false,
-    gender: 'None',
-    categoryId: '',
-    cost_gte: '',
-    cost_lte: '',
-    rating: '',
-    // q: api param for full text search
-    q: null
-  };
-  genders = ['None', 'Man', 'Woman', 'Unisex'];
 
   paginationLinksSubject: BehaviorSubject<{ [s: string]: string }>;
   paginationLinksSubscription;
   paginationLinks: { [s: string]: string };
 
-  fetchProductsTimeout: ReturnType<typeof setTimeout>;
-
   constructor(
     private route: ActivatedRoute,
-    private productsService: ProductsService,
-    private router: Router
+    private productsService: ProductsService
   ) {}
 
   ngOnInit() {
@@ -52,105 +29,31 @@ export class ProductsListComponent implements OnInit, OnDestroy {
       this.products = this.route.snapshot.data.products;
     }
 
-    this.productsService.fetchCategories().subscribe((categoriesData) => {
-      this.categories = categoriesData;
+    this.route.queryParamMap.subscribe((queryParamMap: ParamMap) => {
+      const valuesForFiltering: { [s: string]: any } = {};
+      for (const key of queryParamMap.keys) {
+        valuesForFiltering[key] = queryParamMap.get(key);
+      }
+
+      this.fetchFilteredProducts(valuesForFiltering);
     });
 
     this.getPaginationLinks();
-
-    this.initForm();
   }
 
   ngOnDestroy() {
     this.paginationLinksSubscription.unsubscribe();
   }
 
-  initForm() {
-    this.filtersForm = new FormGroup(
-      {
-        availability: new FormControl(
-          this.route.snapshot.queryParamMap.get('availability') ||
-            this.initialFiltersFormValue.availability
-        ),
-        gender: new FormControl(
-          this.route.snapshot.queryParamMap.get('gender') ||
-            this.initialFiltersFormValue.gender
-        ),
-        categoryId: new FormControl(
-          this.route.snapshot.queryParamMap.get('categoryId') ||
-            this.initialFiltersFormValue.categoryId
-        ),
-        q: new FormControl(
-          this.route.snapshot.queryParamMap.get('q') ||
-            this.initialFiltersFormValue.q
-        ),
-        rating: new FormControl(
-          this.route.snapshot.queryParamMap.get('rating') ||
-            this.initialFiltersFormValue.rating
-        ),
-        cost_gte: new FormControl(
-          this.route.snapshot.queryParamMap.get('cost_gte') ||
-            this.initialFiltersFormValue.cost_gte
-        ),
-        cost_lte: new FormControl(
-          this.route.snapshot.queryParamMap.get('cost_lte') ||
-            this.initialFiltersFormValue.cost_lte
-        )
-      },
-      {
-        validators: [this.costValidator]
-      }
-    );
-
-    // for (const param of this.route.snapshot.queryParamMap.keys) {
-    //   this.filtersForm.patchValue({
-    //     [param]: this.route.snapshot.queryParamMap.get(param)
-    //   });
-    // }
-  }
-
-  onApplyFilters() {
-    console.log(this.filtersForm.value);
-
-    this.fetchFilteredProducts(this.filtersForm.value);
-  }
-
-  onResetFilters() {
-    this.filtersForm.reset(this.initialFiltersFormValue);
-
-    this.fetchFilteredProducts({});
-  }
-
   fetchFilteredProducts(
     valuesForFiltering: { [s: string]: any },
     url?: string
   ) {
-    this.cleanValues(valuesForFiltering);
-
-    this.router.navigate(this.route.snapshot.url, {
-      queryParams: valuesForFiltering
-    });
-
     this.productsService
       .fetchProducts(valuesForFiltering, url)
       .subscribe((productsData: Product[]) => {
         this.products = productsData;
       });
-
-    this.showFilters = false;
-  }
-
-  cleanValues(valuesForFiltering) {
-    for (const key in valuesForFiltering) {
-      if (
-        valuesForFiltering.hasOwnProperty(key) &&
-        (valuesForFiltering[key] === false ||
-          valuesForFiltering[key] === '' ||
-          valuesForFiltering[key] === null)
-      ) {
-        delete valuesForFiltering[key];
-      }
-    }
   }
 
   getPaginationLinks() {
@@ -161,16 +64,5 @@ export class ProductsListComponent implements OnInit, OnDestroy {
         this.paginationLinks = paginationLinks;
       }
     );
-  }
-
-  costValidator(group: FormGroup): { [s: string]: boolean } | null {
-    if (+group.value.cost_gte > +group.value.cost_lte) {
-      group.controls.cost_lte.setErrors({ lesser: "lesser then 'Price from'" });
-      group.controls.cost_gte.setErrors({ greater: "greater then 'Price to'" });
-      return { notvalid: true };
-    }
-    group.controls.cost_lte.setErrors(null);
-    group.controls.cost_gte.setErrors(null);
-    return null;
   }
 }
